322dac1c1ee3ba5695d9c7f65c3cf66adebef938
Hnton <mhinton4@wvup.edu>
2019-01-09 11:03:57 -0500

    For Brian

---
+++ TutoringCenter/TutoringCenter/DAL/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TutoringCenter.Models;
+
+namespace TutoringCenter.DAL
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("ApplicationContext")
+        {
+
+        }
+
+        public DbSet<Login> Logins { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TutoringCenter/TutoringCenter/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TutoringCenter.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public int StudentID { get; set; }
+        public String VisitReason { get; set; }
+        public String Subject { get; set; }
+
+    }
+}
