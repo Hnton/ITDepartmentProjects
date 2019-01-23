9c321665c479d6747e3f54defc2275eea424379a
Brian Belcher <43049467+brianbelcher@users.noreply.github.com>
2019-01-23 11:27:52 -0500

    Changed value of selections

--- TutoringCenter/TutoringCenter/Models/Login.cs
+++ TutoringCenter/TutoringCenter/Models/Login.cs
@@ -23,17 +23,17 @@ namespace TutoringCenter.Models
             {
                 return new List<SelectListItem>
         {
-            new SelectListItem { Text = "Tutoring", Value = "Tutoring"},
-            new SelectListItem { Text = "Printing", Value = "Printing"},
-            new SelectListItem { Text = "Computer Use", Value = "Computer Use"},
-            new SelectListItem { Text = "Study Space", Value = "Study Space"},
-            new SelectListItem { Text = "Math Make-up", Value = "Math Make-up"},
-            new SelectListItem { Text = "Textbook", Value = "Textbook"},
-            new SelectListItem { Text = "Kurzweil", Value = "Kurzweil"},
-            new SelectListItem { Text = "Calculator", Value = "Calculator"},
-            new SelectListItem { Text = "Microscope", Value = "Microscope"},
-            new SelectListItem { Text = "Brainfuse", Value = "Brainfuse"},
-            new SelectListItem { Text = "OTHER", Value = "OTHER"}            
+            new SelectListItem { Text = "Tutoring", Value = "1"},
+            new SelectListItem { Text = "Printing", Value = "2"},
+            new SelectListItem { Text = "Computer Use", Value = "3"},
+            new SelectListItem { Text = "Study Space", Value = "4"},
+            new SelectListItem { Text = "Math Make-up", Value = "5"},
+            new SelectListItem { Text = "Textbook", Value = "6"},
+            new SelectListItem { Text = "Kurzweil", Value = "7"},
+            new SelectListItem { Text = "Calculator", Value = "8"},
+            new SelectListItem { Text = "Microscope", Value = "9"},
+            new SelectListItem { Text = "Brainfuse", Value = "10"},
+            new SelectListItem { Text = "OTHER", Value = "11"}            
         };
             }
         }
@@ -44,20 +44,20 @@ namespace TutoringCenter.Models
             {
                 return new List<SelectListItem>
         {
-            new SelectListItem { Text = "Math", Value = "Math"},
-            new SelectListItem { Text = "English", Value = "English"},
-            new SelectListItem { Text = "Nursing", Value = "Nursing"},
-            new SelectListItem { Text = "Computer Science", Value = "Computer Science"},
-            new SelectListItem { Text = "College 101", Value = "College 101"},
-            new SelectListItem { Text = "Psychology", Value = "Psychology"},
-            new SelectListItem { Text = "History", Value = "History"},
-            new SelectListItem { Text = "Accounting", Value = "Accounting"},
-            new SelectListItem { Text = "Sociology", Value = "Sociology"},
-            new SelectListItem { Text = "Business", Value = "Business"},
-            new SelectListItem { Text = "Criminal Justice", Value = "Criminal Justice"},
-            new SelectListItem { Text = "English (Major)", Value = "English (Major)"},
-            new SelectListItem { Text = "Legal Studies", Value = "Legal Studies"},
-            new SelectListItem { Text = "OTHER", Value = "OTHER"}
+            new SelectListItem { Text = "Math", Value = "1"},
+            new SelectListItem { Text = "English", Value = "2"},
+            new SelectListItem { Text = "Nursing", Value = "3"},
+            new SelectListItem { Text = "Computer Science", Value = "4"},
+            new SelectListItem { Text = "College 101", Value = "5"},
+            new SelectListItem { Text = "Psychology", Value = "6"},
+            new SelectListItem { Text = "History", Value = "7"},
+            new SelectListItem { Text = "Accounting", Value = "8"},
+            new SelectListItem { Text = "Sociology", Value = "9"},
+            new SelectListItem { Text = "Business", Value = "10"},
+            new SelectListItem { Text = "Criminal Justice", Value = "11"},
+            new SelectListItem { Text = "English (Major)", Value = "12"},
+            new SelectListItem { Text = "Legal Studies", Value = "13"},
+            new SelectListItem { Text = "OTHER", Value = "14"}
         };
             }
         }
