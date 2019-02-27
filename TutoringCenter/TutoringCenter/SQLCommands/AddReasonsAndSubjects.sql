4e5e4583a53bfa0e2a78996298b55f2077fce4a3
Hnton <mhinton4@wvup.edu>
2019-02-27 11:33:27 -0500

    SQLCommands

---
+++ TutoringCenter/TutoringCenter/SQLCommands/AddReasonsAndSubjects.sql
@@ -0,0 +1,121 @@
+﻿-- Inserting Reasons into Table
+Insert into Reasons(
+	Name,
+	Status
+	)
+Values
+	(
+		'Tutoring',
+		'False'
+	),
+	(
+		'Printing',
+		'False'
+	),
+	(
+		'Computer Use',
+		'False'
+	),
+	(
+		'Study Space',
+		'False'
+	),
+	(
+		'Math Make-Up',
+		'False'
+	),
+	(
+		'Textbook Use',
+		'False'
+	),
+	(
+		'Box of Bones',
+		'False'
+	),
+	(
+		'Kurzweil',
+		'False'
+	),
+	(
+		'Calculator',
+		'False'
+	),
+	(
+		'Microscope',
+		'False'
+	),
+	(
+		'Brainfuse',
+		'False'
+	),
+	(
+		'Other',
+		'False'
+	);
+
+-- Inserting Subjects into Table
+Insert into Subjects (
+	Name,
+	Status
+	)
+Values
+	(
+		'Math',
+		'False'
+	),
+	(
+		'Science',
+		'False'
+	),
+	(
+		'English',
+		'False'
+	),
+	(
+		'Nursing',
+		'False'
+	),
+	(
+		'Computer Science',
+		'False'
+	),
+	(
+		'College 101',
+		'False'
+	),
+	(
+		'Psychology',
+		'False'
+	),
+	(
+		'History',
+		'False'
+	),
+	(
+		'Accounting',
+		'False'
+	),
+	(
+		'Sociology',
+		'False'
+	),
+	(
+		'Business',
+		'False'
+	),
+	(
+		'Criminal Justice',
+		'False'
+	),
+	(
+		'Education (Major)',
+		'False'
+	),
+	(
+		'Legal Studies',
+		'False'
+	),
+	(
+		'Other',
+		'False'
+	);
